310ebc0d403d2ccbe8a78b120f8ab0e7b12f19f9
AlbertoPlaza <albertoplazamontesdm@gmail.com>
2024-10-05 18:15:52 +0200

    Add: Item documentation

--- src/main/kotlin/wwt/api/controller/ItemController.kt
+++ src/main/kotlin/wwt/api/controller/ItemController.kt
@@ -2,6 +2,12 @@ package wwt.api.controller
 
 import exception.ErrorResponse
 import exception.ResourceNotFoundExcepcion
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -22,21 +28,62 @@ class ItemController(
     private val itemService: ItemService
 ) {
 
-
+    @Operation(
+        summary = "Get an item by id",
+        description = "Get an item by a given id"
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Item found",
+                content = [Content(schema = Schema(implementation = Item::class))]),
+            ApiResponse(responseCode = "404", description = "Item not found",
+                content = [Content(schema = Schema(implementation = ErrorResponse::class))])
+        ]
+    )
     @GetMapping("/getItemById")
-    fun getItemById(@RequestParam id : Int): ResponseEntity<Item> {
+    fun getItemById(
+        @Parameter(description = "The id of the item to be found", example = "1")
+        @RequestParam id : Int
+    ): ResponseEntity<Item> {
         val item = itemService.getItemById(id)
         return ResponseEntity.ok(item)
     }
 
+    @Operation(
+        summary = "Create an item",
+        description = "Create an item with the given data"
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Item created",
+                content = [Content(schema = Schema(implementation = Item::class))]),
+            ApiResponse(responseCode = "400", description = "Bad request")
+        ])
     @PostMapping("/createItem")
-    fun createItem(@RequestBody itemInDto: ItemInDto): ResponseEntity<Item> {
+    fun createItem(
+        @Parameter(description = "The data of the item to be created")
+        @RequestBody itemInDto: ItemInDto
+    ): ResponseEntity<Item> {
         val createdItem = itemService.createItem(itemInDto)
         return ResponseEntity.ok(createdItem)
     }
 
+    @Operation(
+        summary = "Delete an item",
+        description = "Delete an item by a given id"
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "204", description = "Item deleted"),
+            ApiResponse(responseCode = "404", description = "Item not found",
+                content = [Content(schema = Schema(implementation = ErrorResponse::class))])
+        ]
+    )
     @DeleteMapping("/deleteItem")
-    fun deleteItem(@RequestParam id: Int): ResponseEntity<Unit> {
+    fun deleteItem(
+        @Parameter(description = "The id of the item to be deleted", example = "1")
+        @RequestParam id: Int
+    ): ResponseEntity<Unit> {
         itemService.deleteItem(id)
         return ResponseEntity.noContent().build()
     }
--- src/main/kotlin/wwt/api/entity/Item.kt
+++ src/main/kotlin/wwt/api/entity/Item.kt
@@ -1,14 +1,21 @@
 package wwt.api.entity
 
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import org.jetbrains.annotations.NotNull
 
 @Entity
+@Schema(name = "Item", description = "Item entity")
 data class Item(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Item ID", example = "1")
     var id: Int,
+
+    @NotNull
+    @field:Schema(description = "Item ID in minecraft", example = "minecraft:stone")
     var itemId: String
 )
